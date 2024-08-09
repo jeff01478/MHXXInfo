a1b65cfa91a9a5930fae06a2ae780461ecc24b16
jeff01478 <jeff01478@gmail.com>
2024-08-10 00:44:36 +0800

    Develop weaknessTableLayout & Optimize interface

--- app/src/main/java/com/jeff01478/cb/mhxxinfo/activity/MonsterInfoActivity.kt
+++ app/src/main/java/com/jeff01478/cb/mhxxinfo/activity/MonsterInfoActivity.kt
@@ -19,6 +19,7 @@ class MonsterInfoActivity : AppCompatActivity() {
     private lateinit var monsterData: List<Monster>
 
     private lateinit var weaknessTableLayout: TableLayout
+    private lateinit var elementTableLayout: TableLayout
     private lateinit var monsterImageView: ImageView
     private lateinit var monsterNameTextView: TextView
 
@@ -41,51 +42,78 @@ class MonsterInfoActivity : AppCompatActivity() {
         monsterImageView = findViewById(R.id.monsterImageView)
         monsterNameTextView = findViewById(R.id.monsterNameTextView)
         weaknessTableLayout = findViewById(R.id.weaknessTableLayout)
+        elementTableLayout = findViewById(R.id.elementTableLayout)
     }
 
-    private fun setTableLayout() {
+    private fun setWeaknessTableLayout() {
         val headerRow = TableRow(this)
-        headerRow.addView(createTextView("部位", true))
+        headerRow.addView(createTextView("弱點", true))
+        headerRow.addView(createImageView(R.drawable.great_sword))
+        headerRow.addView(createImageView(R.drawable.hammer))
+        headerRow.addView(createImageView(R.drawable.light_bowgun))
+        weaknessTableLayout.addView(headerRow)
+        setWeaknesssDataRow()
+    }
+
+    private fun setWeaknesssDataRow() {
+        for ((bodyPart, weaknessMap) in monsterData[index].weakness) {
+            val dataRow = TableRow(this)
+            dataRow.addView(createTextView(bodyPart, isPart = true))
+            dataRow.addView(createTextView(weaknessMap["cutting"] ?: ""))
+            dataRow.addView(createTextView(weaknessMap["impack"] ?: ""))
+            dataRow.addView(createTextView(weaknessMap["shot"] ?: ""))
+            weaknessTableLayout.addView(dataRow)
+        }
+    }
+
+    private fun setElementTableLayout() {
+        val headerRow = TableRow(this)
+        headerRow.addView(createTextView("屬性弱點", true))
         headerRow.addView(createImageView(R.drawable.fire))
         headerRow.addView(createImageView(R.drawable.water))
         headerRow.addView(createImageView(R.drawable.thunder))
         headerRow.addView(createImageView(R.drawable.ice))
         headerRow.addView(createImageView(R.drawable.dragon))
-//        headerRow.addView(createTextView("火", true))
-//        headerRow.addView(createTextView("水", true))
-//        headerRow.addView(createTextView("雷", true))
-//        headerRow.addView(createTextView("冰", true))
-//        headerRow.addView(createTextView("龍", true))
-        weaknessTableLayout.addView(headerRow)
-        setDataRow()
+        elementTableLayout.addView(headerRow)
+        setElementDataRow()
     }
 
-    private fun setDataRow() {
+    private fun setElementDataRow() {
         for ((bodyPart, weaknessMap) in monsterData[index].weakness) {
             val dataRow = TableRow(this)
             dataRow.addView(createTextView(bodyPart, isPart = true))
-            dataRow.addView(createTextView(weaknessMap["fire"] ?: ""))
-            dataRow.addView(createTextView(weaknessMap["water"] ?: ""))
-            dataRow.addView(createTextView(weaknessMap["thunder"] ?: ""))
-            dataRow.addView(createTextView(weaknessMap["ice"] ?: ""))
-            dataRow.addView(createTextView(weaknessMap["dragon"] ?: ""))
-            weaknessTableLayout.addView(dataRow)
+            dataRow.addView(createTextView(weaknessMap["fire"] ?: "", textColor = Color.RED))
+            dataRow.addView(createTextView(weaknessMap["water"] ?: "", textColor = Color.BLUE))
+            dataRow.addView(createTextView(weaknessMap["thunder"] ?: "", textColor = Color.YELLOW))
+            dataRow.addView(createTextView(weaknessMap["ice"] ?: "", textColor = Color.CYAN))
+            dataRow.addView(createTextView(weaknessMap["dragon"] ?: "", textColor = Color.MAGENTA))
+            elementTableLayout.addView(dataRow)
         }
     }
 
-    private fun createTextView(text: String, isHeader: Boolean = false, isPart: Boolean = false): TextView {
+    private fun createTextView(text: String, isHeader: Boolean = false, isPart: Boolean = false,
+                                textColor: Int = Color.WHITE): TextView {
         val textView = TextView(this)
         textView.apply {
             setText(text)
             setPadding(16, 24, 16, 24)
             gravity = Gravity.CENTER
+            layoutParams = TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f)
             if (isHeader) {
                 setBackgroundColor(Color.GRAY)
+                textSize = 24f
+                gravity = left
             }
             if (isPart) {
                 gravity = left
+                gravity = Gravity.CENTER_VERTICAL
+                layoutParams = TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 4f)
+            }
+            if (isHeader && isPart) {
+                layoutParams = TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT, 4f)
             }
             setTextColor(Color.WHITE)
+            setTextColor(textColor)
         }
         return textView
     }
@@ -94,7 +122,8 @@ class MonsterInfoActivity : AppCompatActivity() {
         val imageView = ImageView(this)
         imageView.apply {
             setImageResource(resourceId)
-            //setPadding(16, 24, 16, 24)
+            setBackgroundColor(Color.GRAY)
+            layoutParams = TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT, 0.25f)
         }
         return imageView
     }
@@ -116,7 +145,8 @@ class MonsterInfoActivity : AppCompatActivity() {
             monsterData = monsters
             index = monsterData.indexOfFirst { it.id == position }
             setMonsterLayout()
-            setTableLayout()
+            setElementTableLayout()
+            setWeaknessTableLayout()
         }
     }
 }
